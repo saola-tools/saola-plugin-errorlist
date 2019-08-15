eae4d500ec113627c72b455ebbe869b4b175cc5a
pnhung177 <pnhung177@acegik.net>
2019-08-15 13:13:01 +0700

    Refactoring the source code

--- test/app/lib/services/example.js
+++ test/app/lib/services/example.js
@@ -17,7 +17,17 @@ function Example(params) {
     errorCodes: sandboxConfig.errorCodes
   });
 
-  console.log('Errorlist: %s', JSON.stringify(errorManager.getAllDescriptors(), null, 2));
+  console.log('=== Errorlist: %s', JSON.stringify(errorManager.getAllDescriptors(), null, 2));
+
+  var myErrorClass = 'UserNotFound';
+  console.log('=== findByErrorClass(%s): %s', myErrorClass,
+    JSON.stringify(errorManager.findByErrorClass(myErrorClass), null, 2)
+  );
+
+  var myReturnCode = 1001;
+  console.log('=== findByReturnCode(%d): %s', myReturnCode,
+    JSON.stringify(errorManager.findByReturnCode(myReturnCode), null, 2)
+  );
 };
 
 Example.referenceHash = {
